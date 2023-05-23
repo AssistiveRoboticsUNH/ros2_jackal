e6c8f8b01024dbf5d3ea0cf9eecbdfeb59e670c9
olaghattas <olaghattas@hotmail.com>
2023-05-22 20:42:07 -0400

    nav

---
+++ jackal_description/launch/description.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    robot_description_command_arg = DeclareLaunchArgument(
+        'robot_description_command',
+        default_value=[
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare('jackal_description'), 'urdf', 'jackal.urdf.xacro']
+            )
+        ]
+    )
+
+    robot_description_content = ParameterValue(
+        Command(LaunchConfiguration('robot_description_command')),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      parameters=[{
+                                          'robot_description': robot_description_content,
+                                      }])
+
+    ld = LaunchDescription()
+    ld.add_action(robot_description_command_arg)
+    ld.add_action(robot_state_publisher_node)
+    return ld
