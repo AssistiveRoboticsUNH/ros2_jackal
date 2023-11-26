8d6b2b38ce7ce60add9f932fba24ad51d6237346
Ola Ghattas <olaghattas@hotmail.com>
2023-11-26 17:13:19 -0500

    minor

--- jackal_navigation/launch/navigation2_jackal.launch.py
+++ jackal_navigation/launch/navigation2_jackal.launch.py
@@ -27,7 +27,7 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    use_rviz = LaunchConfiguration('use_rviz', default='True')
+    use_rviz = LaunchConfiguration('use_rviz', default='False')
     slam = LaunchConfiguration('use_slam', default='False')
 
     # slam = LaunchConfiguration('slam')
